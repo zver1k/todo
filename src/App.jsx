26f80cef45ca43c1888a7fe637772ebf8d745f0c
Damir <mail@damirmedia.ru>
2025-11-14 13:33:59 +0500

    верстка список задач и футера

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import {
+  AiOutlineClose,
   AiOutlineLogin,
   AiOutlineLogout,
   AiOutlinePlus
 } from "react-icons/ai";
+import {useState} from "react";
+import {FiEdit} from "react-icons/fi";
 
 
 function App() {
-
+const [isLogin, setIslogin] = useState(true);
+const [count, setCount] = useState(1);
 
   return (
     <>
@@ -17,10 +21,9 @@ function App() {
           <span className="header__logo-part header__logo-part--second">APP</span>
         </a>
 
-        <div className="header__auth">
-          <AiOutlineLogout />
-          <AiOutlineLogin />
-        </div>
+        {isLogin
+          ? (<a href="#" className="header__auth"><AiOutlineLogout /></a>)
+          : (<a href="#" className="header__auth"><AiOutlineLogin /></a>)}
       </header>
 
       <main className="main">
@@ -28,15 +31,15 @@ function App() {
 
           <div className="main__header">
             <div className="main__header-title">
-              <h2 className="main__title">
-                Выполнено задач
-              </h2>
-              <h3 className="main__subtitle">
-                так держать
-              </h3>
+              <h2 className="main__title">Выполнено задач</h2>
+              {count === 0
+            ?  (<h3 className="main__subtitle">ты сможешь</h3>)
+            :  (<h3 className="main__subtitle">так держать</h3>)}
             </div>
-            <div className="main__tasks">
-              1/3
+
+            {/* Счётчик задач */}
+            <div className="main__tasks-count">
+              {count}/3
             </div>
           </div>
 
@@ -54,6 +57,57 @@ function App() {
             </button>
           </div>
 
+          {/* Список задач */}
+          <ul className="main__tasks-list">
+            <li className="main__task">
+              <button
+                className="main__task-complete"
+                type="button"
+              >
+              </button>
+              <span className="main__task-text">Первая задача</span>
+              <div className="main__task-buttons">
+                <button className="main__task-edit">
+                  <FiEdit />
+                </button>
+                <button className="main__task-remove">
+                  <AiOutlineClose />
+                </button>
+              </div>
+            </li>
+
+            <li className="main__task">
+              <button
+                className="main__task-complete main__task-complete--done"
+                type="button"
+              >
+              </button>
+              <span className="main__task-text">Вторая задача</span>
+              <div className="main__task-buttons">
+                <button className="main__task-edit">
+                  <FiEdit />
+                </button>
+                <button className="main__task-remove">
+                  <AiOutlineClose />
+                </button>
+              </div>
+            </li>
+          </ul>
+          
+          <footer className="footer">
+            <div className="main__footer">
+              <a href="https://damirmedia.ru">
+                <img
+                  className="footer__logo"
+                  src="https://damirmedia.ru/logo.png"
+                  alt="Логотип разработчика DamirMedia"
+                  width="140"
+                  lenght="70"
+                  loading="lazy"
+                />
+              </a>
+            </div>
+          </footer>
 
         </div>
       </main>
